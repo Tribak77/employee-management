5d9bb20a360964082f7d92a46d574b1b8a7cb388
Tribak77 <atribak34@gmail.com>
2024-09-12 10:06:17 +0100

    tuto 2

--- tuto2.js
+++ tuto2.js
@@ -8,10 +8,20 @@ let MyTable = [
 ]
 
 const data = JSON.stringify(MyTable);
+
 fs.writeFile('my_table.json',data, (err)=>{
     if(err){
         console.error(err);
     }else{
         console.log('The table has been saved in my_table');
+    }
+})
+
+fs.readFile('my_table.json','utf8',(err,data)=>{
+    if(err){
+        console.error(err);
+    }else{
+        let MyTable = JSON.parse(data);
+        console.log(MyTable);
     }
 })
